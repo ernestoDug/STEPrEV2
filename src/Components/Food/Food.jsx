f08bf533603e90b7ec6b0eddd33724aab8138780
Ev Bludov <120216330+ernestoDug@users.noreply.github.com>
2024-06-02 21:12:28 +0300

    15

--- src/Components/Food/Food.jsx
+++ src/Components/Food/Food.jsx
@@ -1,25 +1,16 @@
 import { Ul_Style_Food } from "./Food.module";
 
 const Food = () => {
-
-  const food1 = 'Orange';
-  const food2 = 'Banana';
-
+  const food1 = "Orange";
+  const food2 = "Banana";
 
   return (
     <>
       <footer>
         <Ul_Style_Food>
-        <li>
-Apple
-        </li>
-        <li>
-          {food1.toLocaleUpperCase()}
-          </li>
-          <li>
-          {food2}
-          </li>
-
+          <li>Apple</li>
+          <li>{food1.toLocaleUpperCase()}</li>
+          <li>{food2}</li>
         </Ul_Style_Food>
       </footer>
     </>
